f5b69754536bfbdc997d9d0f474867fca548d1b1
Luís Silva <luis.kop@gmail.com>
2018-12-23 17:14:56 +0000

    Added 1.0 version

--- freenas-cli/bin/freenas-cli.sh
+++ freenas-cli/bin/freenas-cli.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Run the java process and pass arguments
-java -jar freenas-cli-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
+java -jar /mnt/c/Users/bastiao/PProj/freenas-java-api-client/freenas-cli/target/freenas-cli-1.0-SNAPSHOT-jar-with-dependencies.jar "$@"
